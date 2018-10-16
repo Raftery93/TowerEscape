2002a21db5f3a16631dbadff15d33c33414ac378
Conor Raftery <c-raftery@hotmail.com>
2018-10-16 21:59:28 +0100

    Boundary Wall auto-generation complete

---
+++ TowerEscape/Assets/__Scripts/BoundaryGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryGenerator : MonoBehaviour {
+
+
+    public GameObject theBoundary;
+    public Transform generationPoint;
+    public float distanceBetween;
+    private float boundaryWidth;
+
+	// Use this for initialization
+	void Start () {
+        boundaryWidth = theBoundary.GetComponent<BoxCollider2D>().size.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(transform.position.y < generationPoint.position.y)
+        {
+            transform.position = new Vector2(transform.position.x,
+                transform.position.y + boundaryWidth + distanceBetween);
+
+            Instantiate(theBoundary, transform.position, transform.rotation);
+        }
+	}
+}
