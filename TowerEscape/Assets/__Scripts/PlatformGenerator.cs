0eebbc1dc41afe9c4685d141ecf8aaca8d6fbbb5
Conor Raftery <c-raftery@hotmail.com>
2018-10-23 19:47:21 +0100

    Platforms & Boundaries generating, player orientated, camera following player

---
+++ TowerEscape/Assets/__Scripts/PlatformGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject thePlatforms;
+    public Transform generationPoint;
+    public float distanceBetween;
+    private float platformWidth;
+
+    // Use this for initialization
+    void Start()
+    {
+        platformWidth = thePlatforms.GetComponent<BoxCollider2D>().size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y < generationPoint.position.y)
+        {
+            transform.position = new Vector2(transform.position.x,
+                transform.position.y + platformWidth + distanceBetween);
+
+            Instantiate(thePlatforms, transform.position, transform.rotation);
+        }
+    }
+}
